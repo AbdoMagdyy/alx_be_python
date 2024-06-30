13de505e52641fe6f6c718b616ae44cee075ba12
abdulrahman <am4011628@gmail.com>
2024-07-01 01:56:53 +0200

    match_case_calculator.py is modified !!

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,12 +1,13 @@
 num1 = int(input('Enter the first number:'))
 num2 = int(input('Enter the second number:'))
 operation = input('Choose the operation (+, -, *, /):')
-if operation == '+':
-    result = num1 + num2
-elif operation == '-':
-    result = num1 - num2 
-elif operation == '*':
-    result = num1 * num2
-elif operation == '/':
-    result = num1 // num2
+match operation:
+    case '+':
+        result = num1 + num2
+    case '-':
+        result = num1 - num2
+    case '*':
+        result = num1 * num2
+    case '/':
+        result = num1 // num2       
 print(f'The result is {result}')
