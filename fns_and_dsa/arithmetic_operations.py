8923646cca56c739d3731daaaced5c7bd6691eae
abdulrahman <am4011628@gmail.com>
2024-07-09 20:33:56 +0200

    arithmetic_operations.py is modified !!!

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,12 +1,16 @@
-def perform_operation(num1 , num2 , operation ):
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
+# arithmetic_operations.py
+
+def perform_operation(num1: float, num2: float, operation: str):
     if operation == 'add':
-        print(num1 + num2)
+        return num1 + num2
     elif operation == 'subtract':
-        print(num1 - num2)
+        return num1 - num2
     elif operation == 'multiply':
-        print(num1 * num2)
+        return num1 * num2
     elif operation == 'divide':
-        print(num1 / num2)
+        if num2 == 0:
+            return "Cannot divide by zero"
+        return num1 / num2
+    else:
+        return "Invalid operation"
+
