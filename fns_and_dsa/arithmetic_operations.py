27d3a6d86e34d49b0291738cead7712a0c9b3d97
abdulrahman <am4011628@gmail.com>
2024-07-10 09:49:16 +0200

    !!!!!!

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,6 @@
-def perform_operation (num1: float, num2: float, operation: str):
+# arithmetic_operations.py
+
+def perform_operation(num1, num2, operation):
     if operation == 'add':
         return num1 + num2
     elif operation == 'subtract':
@@ -11,4 +13,3 @@ def perform_operation (num1: float, num2: float, operation: str):
         return num1 / num2
     else:
         return "Invalid operation"
-
