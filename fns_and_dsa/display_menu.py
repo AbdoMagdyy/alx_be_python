8a58c7aba1dc6e78311dc04d5d776db5304dec4d
abdulrahman <am4011628@gmail.com>
2024-07-12 11:07:55 +0200

    add explore_datetime.py file

--- fns_and_dsa/display_menu.py
+++ fns_and_dsa/display_menu.py
@@ -1,5 +1,4 @@
 import datetime
 from datetime import datetime, timedelta, date, time
-formatted_date = date.today.strftime("%d/%m/%Y")
-print(formatted_date) 
-print(today.strftime("%d/%m/%Y"))
+formatted_date = datetime.today()
+print (formatted_date)
