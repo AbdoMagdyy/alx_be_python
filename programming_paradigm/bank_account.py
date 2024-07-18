8491c3128ba265c2819bd25d7c9bf3f9eb6bd7f7
abdulrahman <am4011628@gmail.com>
2024-07-18 09:00:34 +0200

    fix errors !!

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,19 +1,18 @@
 class BankAccount:
 #start by initializing the attributs
     def __init__(self,initial_balance=0):
-        self.current_balance = initial_balance
+        self.account_balance = initial_balance
 #diposit metod  (it add the money amount to the balance)
     def deposit(self,amount):
         if amount > 0:
-            self.current_balance += amount
+            self.account_balance += amount
         else:
             print('Deposit amount be positive')
 #withdraw method  (it subtract the amount of the money from the balance)
     def withdraw(self, amount):
-        if amount <= self.current_balance:
-            self.balance -= amount
-        else:
-            return False
+        if 0 <amount <= self.account_balance:
+            return True
+        return False
 #display method  (it display the balance of the account)
     def display_balance(self):
-        print(f"Current Balance: {self.current_balance}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
