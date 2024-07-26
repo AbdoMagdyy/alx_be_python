c8d22bc5dfe104b7f2b230aa3997949b29fd69ef
abdulrahman <am4011628@gmail.com>
2024-07-26 22:49:05 +0200

    !

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,25 +1,24 @@
-#starting the (books) list
-books = []
-
 #defining Book class
 class Book:
     def __init__(self,title : str,author : str):
         self.title = title
         self.author = author
-    #defining add_book method **
-    def add_book (self,book):
-        book = input()
-        books.appen(book)
-    #defining list_books
-    def list_books (self):
-        print(books)
+    #showing the results
+    def __str__(self):
+        return f"Book ('{self.title}' , '{self.author}')"
 #defining EBook class
 class EBook(Book):
     def __init__(self, title: str, author: str , file_size : int):
         self.file_size = file_size
         super().__init__(title, author)
+            #showing the results
+    def __str__(self):
+        return f"EBook ('{self.title}' , '{self.author}' , '{self.file_size}')"
 #defining PrintBook class
 class PrintBook(Book):
     def __init__(self, title: str, author: str, page_count : int):
         self.page_count = page_count
         super().__init__(title, author)
+            #showing the results
+    def __str__(self):
+        return f"PrintBook ('{self.title}' , '{self.author}' , '{self.page_count}')"
