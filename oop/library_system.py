5c3f964c8c5be32aa52e5acf01785eba9b9c896f
abdulrahman <am4011628@gmail.com>
2024-07-27 01:19:55 +0200

    !

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,36 +1,47 @@
-#defining Book class
+# Defining Book class
 class Book:
-    def __init__(self,title : str,author : str):
+    def __init__(self, title: str, author: str):
         self.title = title
         self.author = author
-    #showing the results
+
+    # Showing the results
     def __str__(self):
         return f"Book: {self.title} by {self.author}"
-#defining EBook class
+
+# Defining EBook class
 class EBook(Book):
-    def __init__(self, title: str, author: str , file_size : int):
+    def __init__(self, title: str, author: str, file_size: int):
+        super().__init__(title, author)
         self.file_size = file_size
-        super().__init__(title, author)
-            #showing the results
+
+    # Showing the results
     def __str__(self):
-        return f"EBook: {self.title} by {self.author}, File Sie: {self.file_size}"
-#defining PrintBook class
+        return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+
+# Defining PrintBook class
 class PrintBook(Book):
-    def __init__(self, title: str, author: str, page_count : int):
-        self.page_count = page_count
+    def __init__(self, title: str, author: str, page_count: int):
         super().__init__(title, author)
-            #showing the results
+        self.page_count = page_count
+
+    # Showing the results
     def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
-#defining composition class (Library)
+
+# Defining composition class (Library)
 class Library:
-    # initializing attributs
-    def __init__(self, books):
-        self.books = []
-    #defining add_book method
-    def add_book(self,book):
+    # Initializing attributes
+    def __init__(self, books=None):
+        if books is None:
+            self.books = []
+        else:
+            self.books = books
+
+    # Defining add_book method
+    def add_book(self, book):
         self.books.append(book)
-    #defining the list_books function
+
+    # Defining the list_books function
     def list_books(self):
         for book in self.books:
             print(book)
