3c97ace6d6a3496e0e145bf6904a6ad474e01ff7
abdulrahman <am4011628@gmail.com>
2024-07-27 07:53:08 +0200

    depuging !

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -1,10 +1,14 @@
+# Define the Calculator class
 class Calculator:
-    def __init__(self,a,b):
-        self.a = a
-        self.b = b
+    # Class attribute
+    calculation_type = "Arithmetic Operations"
+
+    # Static method for addition
     @staticmethod
-    def add(a,b):
+    def add(a, b):
         return a + b
+
+    # Class method for multiplication
     @classmethod
     def multiply(cls, a, b):
         print(f"Calculation type: {cls.calculation_type}")
